1b2f79cd92a8f1a55846f7ffe0fc360795ccef0b
sreyhani <sajjad7997@gmail.com>
2019-04-25 16:54:38 +0430

    h

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import model.*;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Minion> minions = new ArrayList<Minion>();
         Minion camandareFars = new Minion(101,"camandareFarse",300,2,4,6,SoldierTypes.RANGED,7,null);
-
+        minions.add(camandareFars);
     }
 }
