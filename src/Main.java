061b8efc4527a58b8aed5218e5adaa8bc0b2df20
Amir <a.h.hadian@gmail.com>
2019-04-25 17:19:50 +0430

    committed

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import com.google.gson.Gson;
 import model.*;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        Gson gson = new Gson();
         ArrayList<Minion> minions = new ArrayList<Minion>();
         Minion camandareFars = new Minion(101,"camandareFarse",300,2,4,6,SoldierTypes.RANGED,7,null);
         minions.add(camandareFars);
+        String json = gson.toJson(minions);
+        System.out.println(json);
     }
 }
