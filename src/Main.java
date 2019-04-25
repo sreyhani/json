c369f65557e81b49004b3e4a055b3c1307d5a12a
sreyhani <sajjad7997@gmail.com>
2019-04-25 16:53:43 +0430

    added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import model.*;
 public class Main {
     public static void main(String[] args) {
-        
+        Minion camandareFars = new Minion(101,"camandareFarse",300,2,4,6,SoldierTypes.RANGED,7,null);
+
     }
 }
