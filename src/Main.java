ffbaf20450243226fcd128228226674d4bcae785
sreyhani <sajjad7997@gmail.com>
2019-04-25 17:22:23 +0430

    added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import model.*;
+import model.Buff.Buff;
+import model.Buff.Kind;
+import model.Buff.StunBuff;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Minion> minions = new ArrayList<Minion>();
-        Minion camandareFars = new Minion(101,"camandareFarse",300,2,4,6,SoldierTypes.RANGED,7,null);
+        ArrayList<Buff> buffs = new ArrayList<>();
+        Minion camandareFars = new Minion(101,"camandareFarse",300,2,4,6,SoldierTypes.RANGED,7,null,null);
         minions.add(camandareFars);
+        buffs.add(new StunBuff(Kind.POSITIVE,1,false));
+        Minion shamshirZaneFars = new Minion(102,"shamshir zane fars",400,2,6,4,SoldierTypes.MELEE,0,AbilityCastTime.ON_ATTACK,buffs);
+        minions.add(shamshirZaneFars);
     }
 }
